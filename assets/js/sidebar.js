e4f1086605613a7e5d634ebdd21685e768831324
nhanlnt <nhanlnt@hotmail.com>
2022-10-23 13:39:57 +0700

    feat: created blog content

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,14 +1,41 @@
 $(window).on("load", () => {
-  // add space to top for sidebar.html
-  $("#expandSidebarBtn").css(
-    "top",
-    `${$("#navbarHeader").outerHeight() + 12}px`
-  );
+  const expandSidebarBtn = $("#expandSidebarBtn");
+  const sidebarContainer = $("#sidebarContainer");
+  const sidebarContainerOffsetTop = 108;
+
+  // add space to top for expand sidebar btn
+  expandSidebarBtn.css("top", `${$("#navbarHeader").outerHeight() + 12}px`);
+
+  // add space to top for sidebar
+  const setSidebarTop = () => {
+    if ($(window).innerWidth() >= 992) {
+      sidebarContainer.css(
+        "top",
+        `${
+          $("#blogTitleContainer").outerHeight() + sidebarContainerOffsetTop
+        }px`
+      );
+    } else {
+      sidebarContainer.css("top", `${sidebarContainerOffsetTop}px`);
+    }
+  };
+
+  $(window).resize(() => {
+    setSidebarTop();
+  });
+
+  setSidebarTop();
+
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 0) {
+      sidebarContainer.css("top", `${sidebarContainerOffsetTop}px`);
+    } else {
+      setSidebarTop();
+    }
+  });
 
   // expand sidebar
-  $("#expandSidebarBtn").click(() => {
-    const sidebarContainer = $("#sidebarContainer");
-    const expandSidebarBtn = $("#expandSidebarBtn");
+  expandSidebarBtn.click(() => {
     const activeClass = "active";
     if (!sidebarContainer.hasClass(activeClass)) {
       expandSidebarBtn.addClass(activeClass);
